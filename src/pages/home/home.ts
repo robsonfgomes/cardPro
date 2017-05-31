20ce64da3bdfe28b9e04561ddef7343223893154
robsongomes <robsongomes@ceulp.edu.br>
2017-05-31 15:55:58 -0300

    Adicionada funcionalidade que permite ao usuário criar um novo jogo.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, ToastController, ModalController } from 'ionic-angular';
+import { NavController, ToastController, AlertController } from 'ionic-angular';
 import { Directive } from '@angular/core';
 
 declare var game: any;
@@ -10,11 +10,11 @@ declare var game: any;
 })
 export class HomePage implements OnInit {
 
-  public game;   
-  public initCards = true;  
+  public game;
+  public initCards = true;
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public modalCtrl: ModalController) {
-    this.game = game;        
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+    this.game = game;
     this.game.time = '2:00';
     this.game.hurry = false;
 
@@ -28,14 +28,37 @@ export class HomePage implements OnInit {
   /*
   * Function
   */
-  ngOnInit(){
-      
-   }
+  ngOnInit() {
+
+  }
 
   /*
-  * Implementar modal function
+  * End Game Function
   */
-  
+  showEndGameMessage(title, message, buttonText, endGame = 0): string {
+    if (game.timeOut || endGame == 1) {
+      game.timeOut = false;
+      game.message = '';
+
+      let alert = this.alertCtrl.create({
+        title: title,
+        message: message,
+        buttons: [
+          {
+            text: buttonText,
+            handler: () => {
+              //console.log('New Game as been detected.');
+              location.reload();
+            }
+          }
+        ],
+        enableBackdropDismiss: false
+      });
+      alert.present();
+    }
+    return '';
+  }
+
 
   /*
   * Function
@@ -57,33 +80,30 @@ export class HomePage implements OnInit {
   timeOut(minutes, seconds): void {
     var element, endTime, hours, mins, msLeft, time;
 
-    function twoDigits( n )
-    {
-        return (n <= 9 ? "0" + n : n);
+    function twoDigits(n) {
+      return (n <= 9 ? "0" + n : n);
     }
 
-    function updateTimer()
-    {
-        msLeft = endTime - (+new Date);
-        if(msLeft < 30000) {
-          game.hurry = true;          
-        }          
-        if ( msLeft < 1000 ) {
-            //element.innerHTML = "countdown's over!";
-            game.timeOut = true;
-            game.time = '00:00';
-        } else {
-            time = new Date( msLeft );
-            hours = time.getUTCHours();
-            mins = time.getUTCMinutes();
-            //element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-            game.time = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );                               
-            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-        }
+    function updateTimer() {
+      msLeft = endTime - (+new Date);
+      if (msLeft < 30000) {
+        game.hurry = true;
+      }
+      if (msLeft < 1000) {
+        //"countdown's over!";
+        game.timeOut = true;
+        game.time = '00:00';
+        return;
+      } else {
+        time = new Date(msLeft);
+        hours = time.getUTCHours();
+        mins = time.getUTCMinutes();
+        game.time = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+        setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+      }
     }
 
-    //element = document.getElementById( elementName );
-    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    endTime = (+new Date) + 1000 * (60 * minutes + seconds) + 500;
     updateTimer();
   }
 
